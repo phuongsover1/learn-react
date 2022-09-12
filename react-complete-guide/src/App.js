4febb17bd1386940e63829d53e7b820d52d6c879
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-12 12:49:26 +0700

    First custom component.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
---
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+function ExpenseItem() {
+	return (
+		<div>
+			<div>
+				12 September, 2022
+			</div>
+			<div>
+				<h2>Car Insurance</h2>
+				<div>
+					12$
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ExpenseItem;
