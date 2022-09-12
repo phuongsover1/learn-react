a2c325a9d3e95c18678d3bef639f4d8a57c86667
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-12 20:00:53 +0700

    Used useState() hook to tell React to revaluate Component with new values.

--- react-complete-guide/src/components/Expenses/ExpenseItem.jsx
+++ react-complete-guide/src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+	// let title = props.title;
+	const [title, setTitle] = useState(props.title);
+	const [amount, setAmount] = useState(props.amount);
+	const clickHandler = () => {
+		console.log('Clicked !!!!!');
+		setTitle('Updated!!');
+
+		setAmount(123);
+		// title = 'updated';
+		console.log(title); // old value
+		// title = 'Updated !!!';
+	}
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
-				<div className="expense-item__price">{props.amount}</div>
+				<h2>{title}</h2>
+				<div className="expense-item__price">{amount}</div>
 			</div>
+			<button type="" onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 }
