8554f715b3503f78af69a2ef4ca7a0fc7a8aa7f1
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-14 13:07:37 +0700

    Learned how to specify stateless,stateful components.

--- react-complete-guide/src/components/Expenses/Expense.jsx
+++ react-complete-guide/src/components/Expenses/Expense.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expense.css';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
 function Expense(props) {
+	const [filterYear, setFilterYear] = useState('2020');
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilterYear(selectedYear);
+	};
+
 	return (
 		<Card className="expense">
-			<ExpenseItem
-				title={props.items[0].title}
-				amount={props.items[0].amount}
-				date={props.items[0].date}
-			></ExpenseItem>
+			<ExpenseFilter
+				selected={filterYear}
+				onChangeFilter={filterChangeHandler}
+			/>
 
-			<ExpenseItem
-				title={props.items[1].title}
-				amount={props.items[1].amount}
-				date={props.items[1].date}
-			></ExpenseItem>
-
-			<ExpenseItem
-				title={props.items[2].title}
-				amount={props.items[2].amount}
-				date={props.items[2].date}
-			></ExpenseItem>
-
-			<ExpenseItem
-				title={props.items[3].title}
-				amount={props.items[3].amount}
-				date={props.items[3].date}
-			></ExpenseItem>
+			{props.items.map((expense) => (
+				<ExpenseItem
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+				/>
+			))}
 		</Card>
 	);
 }
