b1472a6a3d3c5a06f32f8065b10efc26a97d638e
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-20 22:13:12 +0700

    Made a re-usable div card.

--- 08-practice-react/01-starting-project/src/components/UI/ErrorModal.jsx
+++ 08-practice-react/01-starting-project/src/components/UI/ErrorModal.jsx
@@ -6,19 +6,17 @@ import Button from "./Button";
 const ErrorModal = (props) => {
 	return (
 		<div className={styles.backdrop}>
-			<div className={styles.modal}>
-				<Card>
-					<div className={styles.header}>
-						<h2 className={styles.header}>Invalid input</h2>
+			<Card className={styles.modal}>
+				<div className={styles.header}>
+					<h2 className={styles.header}>Invalid input</h2>
+				</div>
+				<div className={styles.content}>
+					<p>{props.errorMessage}</p>
+					<div className={styles.actions}>
+						<Button text="Okay" onCloseClick={props.onCloseClick} />
 					</div>
-					<div className={styles.content}>
-						<p>{props.errorMessage}</p>
-						<div className={styles.actions}>
-							<Button text="Okay" onCloseClick={props.onCloseClick} />
-						</div>
-					</div>
-				</Card>
-			</div>
+				</div>
+			</Card>
 		</div>
 	);
 };
