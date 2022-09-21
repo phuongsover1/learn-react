9e826946bbb80bba35eea04850354f5bdaa45c44
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-21 20:13:10 +0700

    Used ReactDOM.createPortals to move component to somewhere else when they are rendered to browser.

--- 08-practice-react/01-starting-project/src/components/UI/ErrorModal.jsx
+++ 08-practice-react/01-starting-project/src/components/UI/ErrorModal.jsx
@@ -2,25 +2,46 @@ import React from "react";
 import styles from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from "react-dom";
+
+const Backdrop = () => {
+	return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<Card className={styles.modal}>
+			<div className={styles.header}>
+				<h2 className={styles.header}>{props.title}</h2>
+			</div>
+			<div className={styles.content}>
+				<p>{props.messages}</p>
+				<div className={styles.actions}>
+					<Button type="button" onCloseClick={props.onCloseClick}>
+						Okay
+					</Button>
+				</div>
+			</div>
+		</Card>
+	);
+};
 
 const ErrorModal = (props) => {
 	return (
-		<div>
-			<div className={styles.backdrop}></div>
-			<Card className={styles.modal}>
-				<div className={styles.header}>
-					<h2 className={styles.header}>{props.title}</h2>
-				</div>
-				<div className={styles.content}>
-					<p>{props.messages}</p>
-					<div className={styles.actions}>
-						<Button type="button" onCloseClick={props.onCloseClick}>
-							Okay
-						</Button>
-					</div>
-				</div>
-			</Card>
-		</div>
+		<React.Fragment>
+			{ReactDOM.createPortal(
+				<Backdrop />,
+				document.getElementById("backdrop-root")
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					title="Invalid Input"
+					messages={props.messages}
+					onCloseClick={props.onCloseClick}
+				/>,
+				document.getElementById("overlay-root")
+			)}
+		</React.Fragment>
 	);
 };
 
