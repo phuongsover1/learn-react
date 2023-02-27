433adc86879da679963dd22ed35aa9ad12dd3a13
Phuong Nguyen <phuongsover2@gmail.com>
2023-02-27 17:03:04 +0700

    Added Meal Item component.

--- 11-practice-project-building-a-food-order-app/src/components/Meals/AvailableMeals.jsx
+++ 11-practice-project-building-a-food-order-app/src/components/Meals/AvailableMeals.jsx
@@ -1,4 +1,6 @@
 import styles from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealsItem/MealItem";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -27,10 +29,19 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ 11-practice-project-building-a-food-order-app/src/components/Meals/MealsItem/MealItem.jsx
@@ -0,0 +1,14 @@
+import styles from "./MealItem.module.css";
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li id={props.id} className={styles.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={styles.description}>{props.description}</div>
+        <div className={styles.price}>{price}</div>
+      </div>
+    </li>
+  );
+};
+export default MealItem;
