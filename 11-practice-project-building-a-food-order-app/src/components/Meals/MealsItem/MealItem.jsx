e9a968b34068937fa12a495ef6af1d6bb5d6e737
Phuong Nguyen <phuongsover2@gmail.com>
2023-02-27 17:39:59 +0700

    Added Input, MealItemForm component.

--- 11-practice-project-building-a-food-order-app/src/components/Meals/MealsItem/MealItem.jsx
+++ 11-practice-project-building-a-food-order-app/src/components/Meals/MealsItem/MealItem.jsx
@@ -1,4 +1,6 @@
+import MealItemForm from "./MealItemForm";
 import styles from "./MealItem.module.css";
+
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
@@ -8,6 +10,7 @@ const MealItem = (props) => {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
+      <MealItemForm />
     </li>
   );
 };
