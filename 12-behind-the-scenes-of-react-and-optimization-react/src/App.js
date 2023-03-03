ef3b26f029db2ec33ceb53e7f637730442fedcce
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-03 11:45:57 +0700

    .

--- 12-behind-the-scenes-of-react-and-optimization-react/src/App.js
+++ 12-behind-the-scenes-of-react-and-optimization-react/src/App.js
@@ -1,22 +1,29 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import Button from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  console.log('APP RUNNING');
+  console.log("APP RUNNING");
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    if (allowToggle)
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, [allowToggle]);
+
+  const allowToggleHander = () => {
+    setAllowToggle(true);
+  };
 
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHander}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
