02ebc1ca4babc517182f33a5ea816a2124fb503a
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-04 17:37:35 +0700

    initial commit.

---
+++ 15-building-custom-react-hooks/src/components/BackwardCounter.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+import Card from "./Card";
+
+const BackwardCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("backward");
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <Card>{counter}</Card>;
+};
+
+export default BackwardCounter;
