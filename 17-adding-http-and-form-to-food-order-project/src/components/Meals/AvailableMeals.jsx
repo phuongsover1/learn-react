905094ec240da15396eba92a44a856fc27f60970
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-09 15:19:43 +0700

    use bind method instead of useCallback

--- 17-adding-http-and-form-to-food-order-project/src/components/Meals/AvailableMeals.jsx
+++ 17-adding-http-and-form-to-food-order-project/src/components/Meals/AvailableMeals.jsx
@@ -1,44 +1,45 @@
-import styles from "./AvailableMeals.module.css";
-import Card from "../UI/Card";
-import MealItem from "./MealsItem/MealItem";
-import { useCallback, useEffect, useState } from "react";
-import useHttp from "../../hooks/use-http";
+import styles from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealsItem/MealItem';
+import { useEffect, useState } from 'react';
+import useHttp from '../../hooks/use-http';
+
+const getTasks = (setMealsList, data) => {
+  let mealsList = [];
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const meal = data[key];
+      mealsList.push({ ...meal, id: key });
+    }
+  }
+  setMealsList(mealsList);
+};
 
 const AvailableMeals = () => {
   const { isLoading, error, sendRequest } = useHttp();
   const [mealsList, setMealsList] = useState([]);
-  const getTasks = useCallback((data) => {
-    let mealsList = [];
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        const meal = data[key];
-        mealsList.push({ ...meal, id: key });
-      }
-    }
-    setMealsList(mealsList);
-  }, []);
   let content = <p>Loading ... </p>;
   useEffect(() => {
     const timer = setTimeout(() => {
       sendRequest(
         {
-          url: "https://react-http-d373b-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json",
+          url: 'https://react-http-d373b-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json',
         },
-        getTasks
+        getTasks.bind(null, setMealsList)
       );
     }, 500);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [sendRequest, getTasks]);
+  }, [sendRequest]);
 
   if (mealsList.length === 0) {
     content = <p>No meals available. Please add some meals </p>;
   }
 
   if (mealsList.length > 0) {
-    content = mealsList.map((meal) => (
+    content = mealsList.map(meal => (
       <MealItem
         id={meal.id}
         name={meal.name}
