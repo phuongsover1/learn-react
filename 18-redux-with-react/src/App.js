4d11779375ac7431c79487891a4b4792014920c0
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-11 10:15:11 +0700

    Using Redux data in react component.

--- 18-redux-with-react/src/App.js
+++ 18-redux-with-react/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Counter from './components/Counter';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 function App() {
-  return <Counter />;
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
 }
 
 export default App;
--- 18-redux-with-react/src/store/index.js
+++ 18-redux-with-react/src/store/index.js
@@ -8,6 +8,4 @@ const counterReducer = (state = { counter: 0 }, action) => {
 
 const store = legacy_createStore(counterReducer);
 
-store.dispatch({ type: 'increment' });
-
 export default store;
