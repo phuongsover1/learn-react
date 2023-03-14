49e3fe0ed65c7ce4910292d81dc1c97dacc6dca2
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-14 22:33:04 +0700

    Exacting Route params.

--- 20-building-a-multi-page-spa-with-react-router/src/pages/ProductDetail.jsx
+++ 20-building-a-multi-page-spa-with-react-router/src/pages/ProductDetail.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 const ProductDetail = () => {
+  const params = useParams();
+  console.log(params.productId);
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
