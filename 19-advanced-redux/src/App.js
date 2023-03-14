62cbb75e73546068aa2f9fb05fb8f6343e5483cb
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-14 14:19:30 +0700

    Handling Http States & Feedback with Redux.

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -1,16 +1,76 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { uiActions } from './store/ui-slice';
+
+let initial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const notification = useSelector(state => state.ui.notification);
+  const cart = useSelector(state => state.cart);
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.setNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data !',
+        })
+      );
+      const response = await fetch(
+        'https://react-http-d373b-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Something wrong happened');
+      }
+
+      dispatch(
+        uiActions.setNotification({
+          status: 'success',
+          title: 'Success!!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    if (initial) {
+      initial = false;
+      return;
+    }
+    sendCartData().catch(error => {
+      dispatch(
+        uiActions.setNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <React.Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </React.Fragment>
   );
 }
 
