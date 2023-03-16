b94f61b954333935e9d65bc26b0c3b7393e9572d
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-16 20:19:13 +0700

    Practicing Nested Routes.

--- 20-building-a-multi-page-spa-with-react-router-1/src/pages/QuoteDetail.jsx
+++ 20-building-a-multi-page-spa-with-react-router-1/src/pages/QuoteDetail.jsx
@@ -1,9 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { Fragment } from 'react';
+import { Route, useParams } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 
 const QuoteDetail = () => {
   const params = useParams();
-  console.log(params.quoteId);
-  return <h1>Quote Detail Page</h1>;
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      {params.quoteId}
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
