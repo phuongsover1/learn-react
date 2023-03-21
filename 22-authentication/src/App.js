64174fd7f518e1cf0bad7fb43dcc79810a90051e
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-21 13:40:14 +0700

    Protecting front-end pages

--- 22-authentication/src/App.js
+++ 22-authentication/src/App.js
@@ -1,11 +1,14 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { AuthContext } from './store/auth-context';
 
 function App() {
+  const ctxObj = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
@@ -13,10 +16,12 @@ function App() {
           <HomePage />
         </Route>
         <Route path='/auth'>
-          <AuthPage />
+          {!ctxObj.isLoggedIn && <AuthPage />}
+          {ctxObj.isLoggedIn && <Redirect to='/' />}
         </Route>
         <Route path='/profile'>
-          <UserProfile />
+          {ctxObj.isLoggedIn && <UserProfile />}
+          {!ctxObj.isLoggedIn && <Redirect to='/auth' />}
         </Route>
       </Switch>
     </Layout>
