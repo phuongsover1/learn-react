d89c9208a10d60bfe19ffda043f3bb0124fcf05e
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-24 01:51:34 +0700

    Handled form submition.

--- learn-formik/simple-form/src/components/YoutubeForm.jsx
+++ learn-formik/simple-form/src/components/YoutubeForm.jsx
@@ -7,12 +7,14 @@ function YoutubeForm() {
       name: 'Phuong',
       email: '',
       channel: ''
+    },
+    onSubmit: values => {
+      console.log('form data', values)
     }
   })
 
-  console.log(formik.values)
   return (
-    <form>
+    <form onSubmit={formik.handleSubmit}>
       <div className='form-control'>
         <label htmlFor="name">Name</label>
         <input type="text" id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
@@ -25,7 +27,7 @@ function YoutubeForm() {
         <label htmlFor="channel">Channel</label>
         <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
       </div>
-      <button>Submit</button>
+      <button type='submit'>Submit</button>
 
 
     </form>
