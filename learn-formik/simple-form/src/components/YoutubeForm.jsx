51f9598fd9180bcec5cc1f6c2bdba7c5288d194d
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-25 17:57:01 +0700

    Working with array values.

--- learn-formik/simple-form/src/components/YoutubeForm.jsx
+++ learn-formik/simple-form/src/components/YoutubeForm.jsx
@@ -12,6 +12,7 @@ const initialValues = {
     facebook: '',
     twitter: '',
   },
+  phoneNumbers: ['', ''],
 };
 
 const onSubmit = values => {
@@ -101,6 +102,18 @@ function YoutubeForm() {
         <div className='form-control'>
           <label htmlFor='twitter'>Twitter</label>
           <Field type='text' id='twitter' name='social.twitter' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='primaryPh'>Primary Phone Number</label>
+          <Field type='text' id='primaryPh' name='phoneNumbers[0]' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='secondaryPh'>Secondary Phone Number</label>
+          <Field
+            type='text'
+            id='secondaryPh'
+            name='phoneNumbers[1]'
+          />
         </div>
         <button type='submit'>Submit</button>
       </Form>
