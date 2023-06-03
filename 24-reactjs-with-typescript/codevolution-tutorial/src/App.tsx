3c8887c85c2b58ae97a6cde7ee8127b8daab6b8a
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-04 05:57:07 +0700

    event props with 'event' parameter

--- 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
+++ 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 import Button from './components/Button';
 
-const handleClick = (): void => console.log('Hello World!!!');
+const handleClick = (
+  id: number,
+  event?: React.MouseEvent<HTMLButtonElement>
+): void => {
+  console.log('id: ', id);
+  console.log('event: ', event?.currentTarget);
+};
+
 function App() {
   return (
     <div className='App'>
-      <Button handleClick={handleClick} />
+      <Button handleClick={handleClick.bind(null, 1)} />
     </div>
   );
 }
