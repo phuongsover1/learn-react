a07fb8316642316f08626ce3ea292193f1d30865
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-04 05:44:05 +0700

    event props without 'event' parameter

--- 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
+++ 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Heading from './components/Heading';
-import Oscar from './components/Oscar';
-import Person from './components/Person';
-import PersonList from './components/PersonList';
-import Status from './components/Status';
+import Button from './components/Button';
 
+const handleClick = (): void => console.log('Hello World!!!');
 function App() {
-  const personName = {
-    lastName: 'Nguyen',
-    firstName: 'Phuong',
-  };
-
-  const personList: { firstName: string; lastName: string }[] = [
-    { firstName: 'Phuong', lastName: 'Nguyen' },
-    { firstName: 'Khang', lastName: 'Nguyen' },
-    { firstName: 'Long', lastName: 'Le' },
-  ];
   return (
     <div className='App'>
-      {/* <Greet name='Phuong' messageCount={15} isLoggedIn={false} /> */}
-      {/* <Person name={personName} /> */}
-      {/* <PersonList names={personList} /> */}
-      {/* <Status status='success' /> */}
-      {/* <Heading>Placeholder Text</Heading> */}
-      {/* <Oscar> */}
-      {/*   <Heading>Oscar goes to Leonardo Dicpario!</Heading> */}
-      {/* </Oscar> */}
-      <Greet name='Luu' isLoggedIn={true} />
+      <Button handleClick={handleClick} />
     </div>
   );
 }
