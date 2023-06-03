3902ba4b42a37aa5fc7c28665cf4e2d987b0ed67
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-04 06:11:26 +0700

    event onchange on input

--- 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
+++ 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button';
+import Input from './components/Input';
 
 const handleClick = (
   id: number,
@@ -10,10 +11,15 @@ const handleClick = (
   console.log('event: ', event?.currentTarget);
 };
 
+const handleChange = (event?: React.ChangeEvent<HTMLInputElement>): void => {
+  console.log('event: ', event?.target.value);
+};
+
 function App() {
   return (
     <div className='App'>
       <Button handleClick={handleClick.bind(null, 1)} />
+      <Input value='asd' handleChange={handleChange} />
     </div>
   );
 }
