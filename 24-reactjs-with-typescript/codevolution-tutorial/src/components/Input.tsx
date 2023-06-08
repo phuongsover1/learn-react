6c1236eb3cdb43453476295137efb98a0febf4b8
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-08 16:55:37 +0700

    Tips using props

--- 24-reactjs-with-typescript/codevolution-tutorial/src/components/Input.tsx
+++ 24-reactjs-with-typescript/codevolution-tutorial/src/components/Input.tsx
@@ -4,8 +4,8 @@ type InputProps = {
   value: string;
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
-const Input = (props: InputProps) => {
-  return <input value={props.value} onChange={props.handleChange.bind(null)} />;
+const Input = ({value, handleChange}: InputProps) => {
+  return <input value={value} onChange={handleChange.bind(null)} />;
 };
 
 export default Input;
