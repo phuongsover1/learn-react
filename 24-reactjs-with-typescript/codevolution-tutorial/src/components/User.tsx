ee3d429bc146ee255639eaa68a36de54b90fe30e
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-08 21:11:08 +0700

    useState future value

---
+++ 24-reactjs-with-typescript/codevolution-tutorial/src/components/User.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+type AuthUser = {
+  name: string; 
+  email: string;
+}
+
+
+const User = () => {
+  const [user, setUser] = useState<null | AuthUser>(null);
+
+  
+
+  const handleLogin = () => {
+    setUser({
+      name: 'Vishwas', 
+      email: 'vishwas@example.com'
+    });
+
+  }
+  const handleLogout = () => {
+    setUser(null);
+  }
+  return (
+    <div>
+    <button onClick={handleLogin}>Login</button>
+    <button onClick={handleLogout}>Logout</button>
+      <div>User name is {user?.name} </div>
+      <div>User name is {user?.email} </div>
+      
+    </div>
+  )
+}
+
+export default User;
