c4e3db06f65e2b3813f5eec8f0343977e710ef64
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-09 14:40:26 +0700

    useReducerHook with props type and reducer type

--- 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
+++ 24-reactjs-with-typescript/codevolution-tutorial/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Button from './components/Button';
 import Container from './components/Container';
+import Counter from './components/Counter';
 import Input from './components/Input';
 
 const handleClick = (
@@ -21,7 +22,8 @@ function App() {
     <div className='App'>
       {/* <Button handleClick={handleClick.bind(null, 1)} /> */}
       {/* <Input value='asd' handleChange={handleChange} /> */}
-      <Container styles={{border: '1px solid black', padding: '1rem'}}  />
+      {/* <Container styles={{border: '1px solid black', padding: '1rem'}}  /> */}
+      <Counter />
     </div>
   );
 }
