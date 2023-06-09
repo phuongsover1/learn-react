c1af2af40b569f394aa28ef3e5451cead9354aac
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-09 15:53:06 +0700

    useReducer Strict Action Types

--- 24-reactjs-with-typescript/codevolution-tutorial/src/components/Counter.tsx
+++ 24-reactjs-with-typescript/codevolution-tutorial/src/components/Counter.tsx
@@ -1,40 +1,48 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
 
 type CounterState = {
   count: number;
-}
+};
 
-type CounterAction = {
-  type: string;
+type UpdateAction = {
+  type: 'increment' | 'decrement';
   payload: number;
-}
+};
 
-  function reducer(state: CounterState, action: CounterAction) {
+type ResetAction = {
+  type: 'reset';
+};
+
+type CounterAction = UpdateAction | ResetAction;
+
+function reducer(state: CounterState, action: CounterAction) {
   switch (action.type) {
     case 'increment':
-      return {count: state.count + action.payload}
+      return { count: state.count + action.payload };
     case 'decrement':
-      return {count: state.count - action.payload};
+      return { count: state.count - action.payload };
+    case 'reset':
+      return initialState;
     default:
       return state;
   }
 }
 
-const initialState = {count: 0};
+const initialState = { count: 0 };
 const Counter = () => {
-
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-       Count: {state.count}
-      <button onClick={() => dispatch({type: 'increment', payload: 10})}>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
         Increment 10
       </button>
-      <button onClick={() => dispatch({type: 'decrement', payload: 10})}>
+      <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </div>
-  )
-}
+  );
+};
 
 export default Counter;
