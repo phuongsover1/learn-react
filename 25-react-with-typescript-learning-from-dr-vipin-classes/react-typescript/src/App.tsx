c2ee7e04971a2a2743c20b768b33c7369f4bd1fa
Phuong Nguyen <phuongsover2@gmail.com>
2023-06-25 12:37:00 +0700

    react setup for typescript.

---
+++ 25-react-with-typescript-learning-from-dr-vipin-classes/react-typescript/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Welcome to Dr. Vipin Classes for learning Reactt with Typescript</h2>
+      <h3>
+        We also have 40+ other playlist for learning Android, Kotlin, React &
+        etc.
+      </h3>
+    </div>
+  );
+}
+
+export default App;
