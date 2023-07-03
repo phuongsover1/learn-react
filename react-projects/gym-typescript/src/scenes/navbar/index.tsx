0fe00cbb294eb372ec02817e7b6f3d60d79e1eb5
Phuong Nguyen <phuongsover2@gmail.com>
2023-07-03 16:32:19 +0700

    MOBILE MENU MODAL

--- react-projects/gym-typescript/src/scenes/navbar/index.tsx
+++ react-projects/gym-typescript/src/scenes/navbar/index.tsx
@@ -58,7 +58,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             ) : (
               <button
                 className="bg rounded-full bg-secondary-500 p-2"
-                onClick={() => setIsMenuToggled(!isMenuToggled)}
+                onClick={() => setIsMenuToggled((prevState) => !prevState)}
               >
                 <Bars3Icon className="h-6 w-6 text-white" />
               </button>
@@ -66,6 +66,39 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+      {/* MOBILE MENU MODAL */}
+      {isMenuToggled && !isAboveMediumScreens && (
+        <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled((prevState) => !prevState)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
