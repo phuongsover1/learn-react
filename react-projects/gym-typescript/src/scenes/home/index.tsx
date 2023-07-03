67ea42b6936bb2f8d517dbbedd67020dc0a91570
Phuong Nguyen <phuongsover2@gmail.com>
2023-07-04 02:05:56 +0700

    complete style home section

---
+++ react-projects/gym-typescript/src/scenes/home/index.tsx
@@ -0,0 +1,80 @@
+import { SelectedPage } from "@/shared/types";
+import React from "react";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  setSelectedPage: (page: SelectedPage) => void;
+};
+
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE AND MAIN HEADER */}
+      <div className="mx-auto w-5/6 md:flex md:h-5/6 md:items-center md:justify-center ">
+        {/* MAIN HEADER */}
+        <div className="z-10 mt-32 md:basis-3/5">
+          {/* HEADINGS */}
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext ">
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+              <p>
+                Unrivaled Gym. Unparalleled Training Fitness Classes. World
+                Class Studios to get the Body Shapes That you Dream of..Get Your
+                Dream Body Now.
+              </p>
+            </div>
+            {/* ACTIONS */}
+            <div className="mt-8 flex items-center gap-8 md:justify-start">
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Join Now
+              </ActionButton>
+              <AnchorLink
+                className="hover: font-bold text-primary-500 underline hover:text-secondary-500"
+                onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                href={`#${SelectedPage.ContactUs}`}
+              >
+                Learn More
+              </AnchorLink>
+            </div>
+          </div>
+        </div>
+        {/* IMAGE */}
+        <div
+          className="md:jml-40 flex basis-3/5 justify-center
+              md:z-10 md:mt-16 md:justify-end"
+        >
+          <img src={HomePageGraphic} alt="home-pageGraphic" />
+        </div>
+      </div>
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div
+              className="flex w-3/5 items-center justify-between
+            gap-8
+            "
+            >
+              <img src={SponsorRedBull} alt="redbull-sponsor" />
+              <img src={SponsorForbes} alt="forbes-sponsor" />
+              <img src={SponsorFortune} alt="fortune-sponsor" />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;
