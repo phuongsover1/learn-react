935abd413b632c7674ad605db2a95b79bcda455c
Phuong Nguyen <phuongsover2@gmail.com>
2023-07-05 17:59:07 +0700

    complete benefits section

--- react-projects/gym-typescript/src/scenes/benefits/index.tsx
+++ react-projects/gym-typescript/src/scenes/benefits/index.tsx
@@ -8,6 +8,8 @@ import {
   UserGroupIcon,
 } from '@heroicons/react/24/solid';
 import Benefit from './Benefit';
+import ActionButton from '@/shared/ActionButton';
+import BenefitsPageGraphic from '@/assets/BenefitsPageGraphic.png';
 
 type Props = {
   setSelectedPage: (page: SelectedPage) => void;
@@ -47,6 +49,14 @@ const Benefits = ({ setSelectedPage }: Props) => {
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
       >
         {/* HEADER */}
         <div className="md:my-5 md:w-3/5">
@@ -76,6 +86,82 @@ const Benefits = ({ setSelectedPage }: Props) => {
           ))}
         </motion.div>
       </motion.div>
+      {/* GRAPHICS AND DESCRIPTION */}
+      <div className="mt-14 items-center justify-between md:mt-24 md:flex">
+        {/* GRAPHIC */}
+        <img
+          className="mx-auto"
+          src={BenefitsPageGraphic}
+          alt="benefits-page-graphic"
+        />
+
+        {/* DESCRIPTION */}
+        <div>
+          {/* TITLE */}
+          <div className="relative">
+            <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] before:content-abstractwaves">
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, x: 50 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+              >
+                <HText>
+                  MILLIONS OF HAPPY MEMBERS GETTING{' '}
+                  <span className="text-primary-500">FIT</span>
+                </HText>
+              </motion.div>
+            </div>
+          </div>
+          {/* DESCRIPT */}
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5, delaly: 0.2 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <p className="my-5">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
+              qui, animi quisquam iste explicabo reprehenderit odio quae
+              laboriosam neque vel rem. Voluptates, recusandae impedit
+              accusantium magnam est cumque obcaecati architecto!
+            </p>
+            <p className="my-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
+              atque ipsa vero, quia repudiandae molestias earum accusantium
+              ullam maxime, officiis obcaecati tempore architecto eligendi! Nam
+              repellat explicabo laudantium. Vero, vitae?
+            </p>
+          </motion.div>
+          {/* BUTTON */}
+          <div className="relative mt-16">
+            <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5, delaly: 0.2 }}
+                variants={{
+                  hidden: { opacity: 0, y: 50 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+              >
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </motion.div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
