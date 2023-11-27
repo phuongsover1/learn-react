a4cb90cd08ad0137ddb9cc42c50707f19ecbd956
Phuong Nguyen <phuongsover2@gmail.com>
2023-11-27 23:47:21 +0700

    using ReactTransitionGroup

--- 24.1-react-with-animations/src/App.js
+++ 24.1-react-with-animations/src/App.js
@@ -1,28 +1,57 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import "./App.css";
-import Modal from "./components/Modal/Modal";
-import Backdrop from "./components/Backdrop/Backdrop";
-import List from "./components/List/List";
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
+import Transition from 'react-transition-group/Transition';
 
 const App = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [showBlock, setShowBlock] = useState(true);
 
   const showModal = () => setModalIsOpen(true);
 
   const closeModal = () => setModalIsOpen(false);
 
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <Modal closed={closeModal} show={modalIsOpen} />
-        <Backdrop show={modalIsOpen} />
-        <button className="Button" type="button" onClick={showModal}>Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
-  }
+  return (
+    <div className='App'>
+      <h1>React Animations</h1>
+      <button onClick={() => setShowBlock(prevState => !prevState)}>
+        Toggle
+      </button>
+      <br />
+      <Transition in={showBlock} timeout={3000} mountOnEnter unmountOnExit>
+        {state => (
+          <>
+            <p>{"Transition's state: " + state}</p>
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'opacity 3s ease-out',
+                opacity: state === 'exiting' || state === 'exited' ? 0 : 1,
+              }}
+            ></div>
+          </>
+        )}
+      </Transition>
+      {modalIsOpen ? (
+        <>
+          <Modal closed={closeModal} show={modalIsOpen} />
+          <Backdrop show={modalIsOpen} />
+        </>
+      ) : null}
 
+      <button className='Button' type='button' onClick={showModal}>
+        Open Modal
+      </button>
+      <h3>Animating Lists</h3>
+      <List />
+    </div>
+  );
+};
 
 export default App;
--- 24.1-react-with-animations/src/components/Backdrop/Backdrop.js
+++ 24.1-react-with-animations/src/components/Backdrop/Backdrop.js
@@ -3,8 +3,11 @@ import React from 'react';
 import './Backdrop.css';
 
 const backdrop = props => {
-  const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
-  return <div className={  cssClasses.join(' ')}></div>;
+  const cssClasses = [
+    'Backdrop',
+    props.show ? 'BackdropOpen' : 'BackdropClosed',
+  ];
+  return <div className={cssClasses.join(' ')}></div>;
 };
 
 export default backdrop;
